18bac84307be5cf457b3d1d187ec15758fd7b73d
ljx <ljxljx1998617@gmail.com>
2023-05-27 07:56:05 +0000

    hh

--- markdown/files/info.md
+++ markdown/files/info.md
@@ -1,9 +1,68 @@
-## welcome to markdown file brewer
+## *welcome to <u>markdown</u> file ~~brewer~~*
 
-the following files written by markdown format,and those will be show up on website by html format.
-* math format:
-$$ y(x,t) = y = A \sin(kx - \omega t) $$
-* code format:
+> the following files written by markdown format,and those will be show up on website by html format.
+
+---
+
+* **header**
+
+1. # header
+
+2. ## header
+
+3. ### header
+
+4. #### header
+
+5. ##### header
+
+6. ###### header
+
+---
+
+* **math format**:
+  $$ y(x,t) = y = A \sin(kx - \omega t) $$
+
+---
+
+* **code format**:
+
 ```c
 #include <stdio.h>
+
+int main(){
+    printf("ljxzdnb!!!");
+}
 ```
+
+---
+
+* **colorfull line**:
+
+  <font color='red'>this is test line!</font><br><font color='blue'>this is test line!!</font>
+
+---
+
+* **images**:
+
+![](./graph.png)
+
+---
+
+* **tables**:
+
+| A1   | B1   | C1   |
+| ---- | ---- | ---- |
+| A2   | B2   | C2   |
+| A3   | B3   | C3   |
+| A4   | B4   | C4   |
+
+---
+
+* **hyperlink**:
+
+[baidu](www.baidu.com)
+
+
+
+<br>
--- markdown/markdown.js
+++ markdown/markdown.js
@@ -11,14 +11,74 @@ angular.module('app').config(function(markedProvider) {
 })
 
 angular.module('app').controller('ctrl', function($scope) {
-    $scope.my_markdown = '## welcome to markdown file brewer\n' +
-        'the following files written by markdown format,and those will be show up on website by html format.\n' +
-        '* math format:\n' +
-        '$$ y(x,t) = y = A \\sin(kx - \\omega t) $$\n' +
-        '* code format:\n' +
+    $scope.my_markdown = '## *welcome to <u>markdown</u> file ~~brewer~~*\n' +
+        '\n' +
+        '> the following files written by markdown format,and those will be show up on website by html format.\n' +
+        '\n' +
+        '---\n' +
+        '\n' +
+        '* **header**\n' +
+        '\n' +
+        '1. # header\n' +
+        '\n' +
+        '2. ## header\n' +
+        '\n' +
+        '3. ### header\n' +
+        '\n' +
+        '4. #### header\n' +
+        '\n' +
+        '5. ##### header\n' +
+        '\n' +
+        '6. ###### header\n' +
+        '\n' +
+        '---\n' +
+        '\n' +
+        '* **math format**:\n' +
+        '  $$ y(x,t) = y = A \\sin(kx - \\omega t) $$\n' +
+        '\n' +
+        '---\n' +
+        '\n' +
+        '* **code format**:\n' +
+        '\n' +
         '```c\n' +
         '#include <stdio.h>\n' +
-        '```'
+        '\n' +
+        'int main(){\n' +
+        '    printf("ljxzdnb!!!");\n' +
+        '}\n' +
+        '```\n' +
+        '\n' +
+        '---\n' +
+        '\n' +
+        '* **colorfull line**:\n' +
+        '\n' +
+        '  <font color=\'red\'>this is test line!</font><br><font color=\'blue\'>this is test line!!</font>\n' +
+        '\n' +
+        '---\n' +
+        '\n' +
+        '* **images**:\n' +
+        '\n' +
+        '![](./graph.png)\n' +
+        '\n' +
+        '---\n' +
+        '\n' +
+        '* **tables**:\n' +
+        '\n' +
+        '| A1   | B1   | C1   |\n' +
+        '| ---- | ---- | ---- |\n' +
+        '| A2   | B2   | C2   |\n' +
+        '| A3   | B3   | C3   |\n' +
+        '| A4   | B4   | C4   |\n' +
+        '\n' +
+        '---\n' +
+        '\n' +
+        '* **hyperlink**:\n' +
+        '\n' +
+        '[baidu](www.baidu.com)\n' +
+        '\n' +
+        '\n' +
+        '\n' +
+        '<br>'
     function enableTab(id) {
         var el = document.getElementById(id);
         el.addEventListener('input', function(e) {
