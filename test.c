464e5846c07a3eb415eb8c54cb2002b4ea198381
ljxnb <ljxljx1998617@gmail.com>
2024-02-29 13:28:38 +0000

    hh

--- test.c
+++ test.c
@@ -1,41 +1,49 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdbool.h>
 
-int main() {
-    char str[1001];
-    char* strs[32];
-    while (fgets(str, sizeof(str), stdin) != NULL) {
-        int len = strlen(str);
-        int count = 0;
-        bool inQuotes = false;
 
-        // 替换空格为换行符
-        for (int i = 0; i < len; ++i) {
-            if (str[i] == '"') {
-                inQuotes = !inQuotes;
-            } else if (str[i] == ' ' && !inQuotes) {
-                str[i] = '\n';
-            }
-        }
 
-        // 统计参数个数并输出
-        char* token = strtok(str, "\n");
-        while (token != NULL) {
-            // 去除引号
-            strs[count] = token;
-            ++count;
-            token = strtok(NULL, "\n");
-        }
-        printf("%d\n", count);
-        for(int i=0;i<count;i++){
-            if (strs[i][0] == '"' && strs[i][strlen(strs[i]) - 1] == '"') {
-                printf("%.*s\n", (int)strlen(strs[i]) - 2, strs[i] + 1);
-            } else {
-                printf("%s\n", strs[i]);
-            }
+struct poker{
+    char cards[16];
+    char card[6][6];
+    int type;
+    int value;
+}cardA,cardB;
+
+char *types[15] = {"3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A", "2", "joker", "JOKER"};
+
+
+void valuedec(struct poker* tmp){
+    if(strstr(tmp->cards,"joker JOKER") || strstr(tmp->cards,"JOKER joker")){
+        tmp->type = 0;
+        return;
+    }
+    for(int i =0;i<tmp->type;i++){
+        for (int j= 0; j < 15; j++) {
+            if (strcmp(tmp->card[i], types[j]) == 0) 
+                tmp->value+=j;
         }
     }
-    return 0;
 }
 
+int main() {
+
+    // 输入两手牌
+    scanf("%[^-]", cardA.cards);
+    getchar(); // 消耗换行符
+    scanf("%[^\n]", cardB.cards);
+
+    cardA.type = sscanf(cardA.cards,"%s%s%s%s%s", cardA.card[0], cardA.card[1], cardA.card[2], cardA.card[3], cardA.card[4]);
+    cardB.type = sscanf(cardB.cards,"%s%s%s%s%s", cardB.card[0], cardB.card[1], cardB.card[2], cardB.card[3], cardB.card[4]);
+    valuedec(&cardA);
+    valuedec(&cardB);
+    if(cardA.type == cardB.type)
+        printf("%s\n",cardA.value > cardB.value?cardA.cards:cardB.cards);
+    else if(cardA.type == 0 || cardB.type == 0 )
+       printf("%s\n",cardA.type == 0?cardA.cards:cardB.cards);
+    else if(cardA.type == 4 || cardB.type == 4 )
+        printf("%s\n",cardA.type == 4?cardA.cards:cardB.cards);
+    else
+        printf("ERROR");
+    return 0;
+}
