c211661cd23edd3e66bc5a427e6a9ff94df74ecf
ljxnb <ljxljx1998617@gmail.com>
2024-03-05 06:01:06 +0000

    hh

--- test.c
+++ test.c
@@ -1,36 +1,71 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-struct recordline{
-    char name[128];
-    int line;
-    int num;
-};
+typedef struct CH Ch;
+struct CH {
+    char* ch;
+    Ch* next;
+    /* data */
+}* header, *ptr;
+
+void nodelink(Ch* sec, int index, char* str) {
+    ptr = &sec[index];
+    *ptr = (Ch) {
+        str, header
+    };
+    header = ptr;
+}
 
 int main() {
-    int i=0,index = 0;
-    char name[128] = {0};
-    struct recordline tmp ={{0},0,1};
-    struct recordline record[128] = {0};
-    while (scanf("%s %d", name, &tmp.line) == 2) { // 注意 while 处理多个 case
-        char* sec = strtok(name, "\\"),*old=NULL;
-        while (sec) {
-            old = sec;
-            sec = strtok(NULL, "\\");
+    char number[][4] = {"零", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖"};
+    char unit[][4] = {"分", "角", "元", "拾", "佰", "仟", "万", "拾", "佰", "仟", "亿"};
+    int num = 0;
+    double value;
+    int zero = 0;
+    int posnum = 0;
+    while (scanf("%lf", &value) != EOF) {
+        Ch sec[128] = {0};
+        int index = 0;
+        int position = 0;
+        num = (value + 0.005) * 100;
+        zero = 0x02;
+        while (num) {
+            if (!header && position > 1)
+                nodelink(sec, index++, "整");
+            posnum = num % 10;
+            if ((0x01 & zero) || posnum)
+                nodelink(sec, index++, unit[position]);
+            switch (posnum) {
+                case 0:
+                    if (!zero && position > 1)
+                        nodelink(sec, index++, number[posnum]);
+                    break;
+                case 1:
+                    if (position != 3 && position != 7)
+                        nodelink(sec, index++, number[posnum]);
+                    break;
+                default:
+                    nodelink(sec, index++, number[posnum]);
+            }
+            num /= 10;
+            position++;
+            if (posnum)
+                zero &= 0xfd;
+            else
+                zero |= 0x02;
+            if ((position + 2) % 4 == 0)
+                zero |= 0x01;
+            else
+                zero &= 0xfe;
         }
-        strcpy(tmp.name, old);
-        for(i = 0;i<index+1;i++){
-            if(tmp.line == record[i].line && strcmp(tmp.name,record[i].name)==0){
-                record[i].num++;
-                index--;
-                break;
-            }else if(i == index)
-                record[i] = tmp;
+        printf("人民币");
+        while (header) {
+            ptr = header->next;
+            printf("%s", header->ch);
+            header = ptr;
         }
-        index++;
+        printf("\n");
     }
-    for(i = index - 8 > 0 ? index - 8 : 0; i < index; i++)
-        printf("%s %d %d\n",&record[i].name[(int)strlen(record[i].name) - 16 >0?strlen(record[i].name)-16:0],record[i].line,record[i].num);
-    
     return 0;
 }
